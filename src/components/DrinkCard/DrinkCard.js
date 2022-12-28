09648abfe360ab5706831baa23fa1d24d24c70c6
Klaudia Stanczyk <klaudia.stanczyk92@gmail.com>
2022-12-28 13:35:25 +0100

    card name style

--- src/components/DrinkCard/DrinkCard.js
+++ src/components/DrinkCard/DrinkCard.js
@@ -1,11 +1,13 @@
-import * as S from './DrinkCard.styles';
+import * as S from "./DrinkCard.styles";
 
-export const DrinkCard = ({name, ingredients, price}) => {
+export const DrinkCard = ({ name, ingredients, price }) => {
   return (
     <S.DrinkCard>
-      <S.NameContainer>{name}</S.NameContainer>
+      <S.NameContainer>
+        <S.NameText>{name}</S.NameText>
+      </S.NameContainer>
       <S.IngredientsContainer>{ingredients}</S.IngredientsContainer>
       <S.PriceContainer>{price}</S.PriceContainer>
     </S.DrinkCard>
-  )
-}
+  );
+};
--- src/components/DrinkCard/DrinkCard.styles.js
+++ src/components/DrinkCard/DrinkCard.styles.js
@@ -3,10 +3,13 @@ import styled from "styled-components";
 export const DrinkCard = styled.div`
   display: flex;
   flex-direction: column;
+  border: 1px solid white;
+  border-radius: 5px;
+  width: 30%;
+  margin: 20px 20px;
 `
 
 export const NameContainer = styled.div`
-  border: 1px solid black;
   margin-bottom: 10px;
 `
 
@@ -17,4 +20,10 @@ export const IngredientsContainer = styled.div`
 export const PriceContainer = styled.div`
   border: 1px solid black;
   margin-bottom: 10px;
+`
+export const NameText = styled.h1`
+  font-size: 15px;
+  font-family: 'Raleway', sans-serif;
+  color: white;
+  margin-left: 5px;
 `
